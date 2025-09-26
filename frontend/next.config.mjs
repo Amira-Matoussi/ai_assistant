6db1f08332320e3c7cbee05db9d949c147133809
Amira <amiramatoussi310@gmail.com>
2025-09-26 13:52:19 +0100

    conf changes made

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,5 +1,6 @@
 import path from "path";
 import { fileURLToPath } from "url";
+import { fa } from "zod/v4/locales";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -7,17 +8,18 @@ const __dirname = path.dirname(__filename);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
-    ignoreDuringBuilds: true,
+    ignoreDuringBuilds: false,
   },
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: false,
   },
   images: {
     unoptimized: true,
   },
   webpack: (config) => {
     // ✅ Use absolute path so alias works in Vercel too
-    config.resolve.alias["@"] = path.resolve(__dirname);
+    //config.resolve.alias["@"] = path.resolve(__dirname);
+    config.resolve.alias["@"] = path.join(__dirname, "src");
     return config;
   },
 };
